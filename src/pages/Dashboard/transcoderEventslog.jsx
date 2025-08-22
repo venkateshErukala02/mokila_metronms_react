e06b767b5d8c1e2fe34010d2a105f1118472bbf1
venkateshErukala02 <venkateshe0219@gmail.com>
2025-08-22 07:46:55 -0400

    made changes not to call twice getapi

--- src/pages/Dashboard/transcoderEventslog.jsx
+++ src/pages/Dashboard/transcoderEventslog.jsx
@@ -7,7 +7,6 @@ import '../Navbar/navbarpage.css';
 import { index } from "d3";
 import { useSelector } from "react-redux";
 import DatePicker from "react-datepicker";
-// import TimePicker from 'react-time-picker';
 
 
 
@@ -22,13 +21,8 @@ const TranscoderEventLog = ({ currentTab, nodeItemDt }) => {
             return `${y}-${month}-${day}`;
         } catch (error) {
             console.error('Error formatting date:', error.message);
-            return null; // or a fallback value
+            return null;
         }
-        //   const year = d.getFullYear();
-        //   const month = String(d.getMonth() + 1).padStart(2, '0'); // months are 0-based
-        //   const day = String(d.getDate()).padStart(2, '0');
-
-
     }
 
 
@@ -41,6 +35,9 @@ const TranscoderEventLog = ({ currentTab, nodeItemDt }) => {
     const [selectLog, setSelectLog] = useState('obclogs');
     const [searchText, setSearchText] = useState('');
     const [search, setSearch] = useState('');
+    const [executedSearch, setExecutedSearch] = useState("");
+    const [executedDate, setExecutedDate] = useState("");
+    const [searchTrigger, setSearchTrigger] = useState(0);
     const nodeDataId = useSelector((state) => state.node.node.nodeId) || localStorage.getItem('nodeId');
     const nodeIpaddress = useSelector((state) => state.node.node.ipAddress) || localStorage.getItem('nodeIpaddress');
 
@@ -89,149 +86,64 @@ const TranscoderEventLog = ({ currentTab, nodeItemDt }) => {
     }
 
 
-
-
-    useEffect(() => {
-        let url = "";
-
-        if (currentTab === 'obc') {
-            const targetDate = date || selectedDate;
-            if (!targetDate) return;
-
-            const y = targetDate.getFullYear();
-            const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-            const day = String(targetDate.getDate()).padStart(2, '0');
-            const formattedDate = `${y}-${month}-${day}`;
-
-            if (selectLog === 'obclogs') {
-                const targetDate = date || selectedDate;
-                if (!targetDate) return;
-
-                const y = targetDate.getFullYear();
-                const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-                const day = String(targetDate.getDate()).padStart(2, '0');
-                const hours = String(targetDate.getHours()).padStart(2, '0');
-                const minutes = String(targetDate.getMinutes()).padStart(2, '0');
-                const seconds = String(targetDate.getSeconds()).padStart(2, '0');
-                const obcFormatDate = `${y}${month}${day}/${hours}${minutes}${seconds}`;
-                url = `api/v2/treeview/datelogs/${nodeItemDt?.carnumber || ''}/${obcFormatDate}?q=${search}`;
-            } else if (selectLog === 'trainlogs') {
-                url = `api/v2/treeview/trainlogs/${nodeItemDt?.carnumber || ''}/${formattedDate}?q=${search}`;
-            }
-        } else {
-            const targetDate = date || selectedDate;
-            if (!targetDate) return;
-
-            const y = targetDate.getFullYear();
-            const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-            const day = String(targetDate.getDate()).padStart(2, '0');
-            const formattedDate = `${y}-${month}-${day}`;
-            url = `http://${nodeIpaddress}:8084/${currentTab}/api/v1/logs?since=${formattedDate}`;
-        }
-
-        if (url) {
-            getTranscoderLogData(url);
-        }
-    }, [date, selectLog, nodeIpaddress, currentTab, nodeItemDt]);
-    
-    useEffect(() => {
-        setSearchText('')
-        setSearch('')
-    }, [selectLog])
-
-    useEffect(() => {
-        // if (!searchText) return;
-        let url = '';
-        if (currentTab === 'transcoder') {
-            const targetDate = date || selectedDate;
-            if (!targetDate) return;
-
-            const y = targetDate.getFullYear();
-            const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-            const day = String(targetDate.getDate()).padStart(2, '0');
-            const formattedDate = `${y}-${month}-${day}`;
-            url = `http://${nodeIpaddress}:8084/${currentTab}/api/v1/logs?since=${formattedDate}&search=%27${search}%27`
-        }
-          else if(currentTab === 'obc' && selectLog === 'obclogs'){
-             const targetDate = date || selectedDate;
-                if (!targetDate) return;
-
-                const y = targetDate.getFullYear();
-                const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-                const day = String(targetDate.getDate()).padStart(2, '0');
-                const hours = String(targetDate.getHours()).padStart(2, '0');
-                const minutes = String(targetDate.getMinutes()).padStart(2, '0');
-                const seconds = String(targetDate.getSeconds()).padStart(2, '0');
-                const obcFormatDate = `${y}${month}${day}/${hours}${minutes}${seconds}`;
-
-            url=`api/v2/treeview/datelogs/${nodeItemDt?.carnumber || ''}/${obcFormatDate}?q=${search}`
-          }else if(currentTab ==='obc' && selectLog === 'trainlogs' ){
-
-             const targetDate = date || selectedDate;
-            if (!targetDate) return;
-
-            const y = targetDate.getFullYear();
-            const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-            const day = String(targetDate.getDate()).padStart(2, '0');
-            const formattedDate = `${y}-${month}-${day}`;
-
-            url=`api/v2/treeview/trainlogs/${nodeItemDt?.carnumber || ''}/${formattedDate}?q=${search}`
-          }
-        getTranscoderLogData(url);
-    }, [search]);
-
-    const handleDatePicker = (value) => {
-        //    setDate(value)
-    }
-    const handleSearch = (searchText, selectedDate) => {
-        setSearch(searchText);
-        setDate(selectedDate);
-        let url=''
-        if (currentTab === 'obc') {
-            const targetDate = date || selectedDate;
-            if (!targetDate) return;
-
-            const y = targetDate.getFullYear();
-            const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-            const day = String(targetDate.getDate()).padStart(2, '0');
-            const formattedDate = `${y}-${month}-${day}`;
-
-            if (selectLog === 'obclogs') {
-                const targetDate = date || selectedDate;
-                if (!targetDate) return;
-
-                const y = targetDate.getFullYear();
-                const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-                const day = String(targetDate.getDate()).padStart(2, '0');
-                const hours = String(targetDate.getHours()).padStart(2, '0');
-                const minutes = String(targetDate.getMinutes()).padStart(2, '0');
-                const seconds = String(targetDate.getSeconds()).padStart(2, '0');
-                const obcFormatDate = `${y}${month}${day}/${hours}${minutes}${seconds}`;
-                url = `api/v2/treeview/datelogs/${nodeItemDt?.carnumber || ''}/${obcFormatDate}?q=${search}`;
-            } else if (selectLog === 'trainlogs') {
-                url = `api/v2/treeview/trainlogs/${nodeItemDt?.carnumber || ''}/${formattedDate}?q=${search}`;
-            }
-        } else if (currentTab === 'transcoder') {
-            const targetDate = date || selectedDate;
-            if (!targetDate) return;
-
-            const y = targetDate.getFullYear();
-            const month = String(targetDate.getMonth() + 1).padStart(2, '0');
-            const day = String(targetDate.getDate()).padStart(2, '0');
-            const formattedDate = `${y}-${month}-${day}`;
-            url = `http://${nodeIpaddress}:8084/${currentTab}/api/v1/logs?since=${formattedDate}&search=%27${search}%27`
-
-        }
-
-
-        if (url) {
-            getTranscoderLogData(url);
-        }
-    }
     const handleClearSearch = () => {
         setSearchText('');
         setSearch('');
     }
+
+    const handleSearch = (searchText, selectedDate) => {
+  setExecutedSearch(searchText);
+  setExecutedDate(selectedDate);
+  setSearchTrigger(prev => prev + 1); 
+};
+
+    useEffect(() => {
+        setSearchText('');
+        setSearch('');
+        setExecutedDate(new Date());
+        setExecutedSearch('');
+    }, [selectLog])
+
+
+useEffect(() => {
+
+  let url = "";
+
+  if (currentTab === "obc") {
+    const targetDate = executedDate || selectedDate;
+    if (!targetDate) return;
+
+    const y = targetDate.getFullYear();
+    const month = String(targetDate.getMonth() + 1).padStart(2, "0");
+    const day = String(targetDate.getDate()).padStart(2, "0");
+    const formattedDate = `${y}-${month}-${day}`;
+
+    if (selectLog === "obclogs") {
+      const hours = String(targetDate.getHours()).padStart(2, "0");
+      const minutes = String(targetDate.getMinutes()).padStart(2, "0");
+      const seconds = String(targetDate.getSeconds()).padStart(2, "0");
+      const obcFormatDate = `${y}${month}${day}/${hours}${minutes}${seconds}`;
+      url = `api/v2/treeview/datelogs/${nodeItemDt?.carnumber || ""}/${obcFormatDate}?q=${executedSearch}`;
+    } else if (selectLog === "trainlogs") {
+      url = `api/v2/treeview/trainlogs/${nodeItemDt?.carnumber || ""}/${formattedDate}?q=${executedSearch}`;
+    }
+  } else {
+    const targetDate = executedDate || selectedDate;
+    if (!targetDate) return;
+
+    const y = targetDate.getFullYear();
+    const month = String(targetDate.getMonth() + 1).padStart(2, "0");
+    const day = String(targetDate.getDate()).padStart(2, "0");
+    const formattedDate = `${y}-${month}-${day}`;
+    url = `http://${nodeIpaddress}:8084/${currentTab}/api/v1/logs?since=${formattedDate}&search=%27${executedSearch}%27`;
+    
+  }
+
+  if (url) {
+    getTranscoderLogData(url);
+  }
+}, [executedSearch, executedDate, searchTrigger, nodeIpaddress, currentTab, nodeItemDt]);
+
 
 
 
