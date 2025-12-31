fb8cd11a74f17dc58a439ba8b11bd6a92f411f61
venkateshErukala02 <venkateshe0219@gmail.com>
2025-12-31 07:11:31 -0500

    Added export cab ui in events page.

--- src/pages/Events/eventspage.jsx
+++ src/pages/Events/eventspage.jsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import EventMainTB from "./evntsmaintb";
 import '../ornms.css';
 import './../Events/events.css';
+import DatePicker from "react-datepicker";
 
 
 const EventPg = () => {
@@ -19,9 +20,45 @@ const EventPg = () => {
                   </article>
                   <article className="container-fluid">
                   <article className="row">
-                    <article className="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <article className="col-sm-10 col-md-10 col-lg-10 col-xl-10 col-xxl-10" style={{paddingRight:'10px'}}>
                         <h1 className="evntsheadcl">Events</h1>
                         <EventMainTB/>
+                    </article>
+                     <article className="col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2" style={{padding:'8px 13px 0 13px'}}>
+                        <article className="border-allsd exportcabcolheight">
+                        <h1 className="evntsheadcl border-allsd">Export Cab Logs</h1>
+                        <article style={{padding:"12px"}}>
+                            <label className="settinglabelsub">Cab Number</label>
+                            <input type="text"
+                                // value={version}
+                                required
+                                // onChange={(e) => setVersion(e.target.value)}
+                                name="" placeholder="" id="" className="settinglabelsubinp" />
+                                <article className="labelaligncl">
+                                <label className="settinglabelsub">From</label>
+                            <DatePicker
+                            // selected={selectedDate}
+                            showTimeSelect
+                            dateFormat="yyyy-MM-dd HH:mm"
+                            placeholderText="yyyy-MM-dd HH:mm"
+                                // onChange={handleDateChange}
+                            className="myDatepickercl" />
+                            </article>
+                            <article className="labelaligncl">
+                                <label className="settinglabelsub">To</label>
+                           <DatePicker
+                            // selected={selectedDate}
+                            showTimeSelect
+                            dateFormat="yyyy-MM-dd HH:mm"
+                            placeholderText="yyyy-MM-dd HH:mm"
+                                // onChange={handleDateChange}
+                            className="myDatepickercl" />
+                            </article>
+                                <article className="f-r labelaligncl">
+                                    <button type="button" className="createbtn">Export</button>
+                                </article>
+                        </article>
+                        </article>
                     </article>
                     </article>
                 </article>
